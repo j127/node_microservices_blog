77717839899c35147130dfc56de5e3224f83b5fc
J. Cohen <hostelmarketing@gmail.com>
2020-05-11 19:42:37 -0700

    Frontend to create and lists posts

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Node Microservices Exercise
-
-This is the first project from a course on [Node microservices](https://www.udemy.com/course/microservices-with-node-js-and-react/).
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 export default () => {
     return (
         <div className="container">
-            <h1>Create Post</h1>
+            <h1>Blog</h1>
+            <h2>Create Post</h2>
             <PostCreate />
+            <hr />
+            <h2>Posts</h2>
+            <PostList />
         </div>
     );
 };
