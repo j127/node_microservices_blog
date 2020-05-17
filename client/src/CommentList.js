ed8de1c93fcf8107e3e2becff03c7c6f1884ab49
J. Cohen <hostelmarketing@gmail.com>
2020-05-16 21:26:37 -0700

    Update client to show moderated comments.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 export default ({ comments }) => {
-    const renderedComments = comments.map(c => <li key={c.id}>{c.content}</li>);
+    const renderedComments = comments.map(c => {
+        let content;
+
+        if (c.status === "approved") {
+            content = c.content;
+        }
+
+        if (c.status === "pending") {
+            content = "This comment is awaiting moderation.";
+        }
+
+        if (c.status === "rejected") {
+            content = "This comment has been rejected.";
+        }
+
+        return <li key={c.id}>{content}</li>;
+    });
 
     return (
         <div>
