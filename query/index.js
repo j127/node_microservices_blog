5d5fab1ff2ee01cddf7608cc14bd947c9702040a
J. Cohen <hostelmarketing@gmail.com>
2020-05-16 21:53:25 -0700

    Store the events for query service.
    If the query service goes offline
    it can still get the previous events
    when it boots up.

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -8,19 +9,7 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-// logger
-app.use((req, _res, next) => {
-    console.log(new Date(), req.method, req.url, req.body);
-    next();
-});
-
-app.get("/posts", (_req, res) => {
-    res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === "PostCreated") {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -44,13 +33,31 @@ app.post("/events", (req, res) => {
         comment.content = content;
         console.log("changed status because CommentUpdated", comment);
     }
+};
 
-    console.log("[[query]] posts", posts);
+// logger
+app.use((req, _res, next) => {
+    console.log(new Date(), req.method, req.url, req.body);
+    next();
+});
 
+app.get("/posts", (_req, res) => {
+    res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
     res.send({});
 });
 
 const PORT = 4002;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`listening at http://localhost:${PORT}`);
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+        console.log("processing event", event.type);
+        handleEvent(event.type, event.data);
+    }
 });
