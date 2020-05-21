ede3407229bf5cd9841ced8fe501e277fbee2130
J. Cohen <hostelmarketing@gmail.com>
2020-05-21 02:30:21 -0700

    Link the first two kubernetes pods

--- event_bus/index.js
+++ event_bus/index.js
@@ -13,17 +13,17 @@ app.post("/events", (req, res) => {
 
     events.push(event);
 
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
-    axios.post("http://localhost:4003/events", event);
+    axios.post("http://posts-clusterip-srv:4000/events", event);
+    // axios.post("http://localhost:4001/events", event);
+    // axios.post("http://localhost:4002/events", event);
+    // axios.post("http://localhost:4003/events", event);
 
     res.send({ status: "OK" });
 });
 
-app.get("/events", (req, res) => {
+app.get("/events", (_req, res) => {
     res.send(events);
 });
 
 const PORT = 4005;
-app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- posts/index.js
+++ posts/index.js
@@ -29,7 +29,7 @@ app.post("/posts", async (req, res) => {
         title,
     };
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
         type: "PostCreated",
         data: {
             id,
@@ -47,6 +47,5 @@ app.post("/events", (req, res) => {
 
 const PORT = process.env.POSTS_PORT || 4000;
 app.listen(PORT, () => {
-    console.log("v55");
-    console.log(`listening at http://localhost:${PORT}`);
+    console.log(`listening on port ${PORT}`);
 });
